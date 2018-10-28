bdd0255a87a9269257bb97e98b47020ba85f9b6e
Jay Ward <jay@paladinarcher.com>
2018-10-28 14:54:51 -0600

    fixing Swift class

--- src/ios/BackgroundTimer.swift
+++ src/ios/BackgroundTimer.swift
@@ -1,97 +1,95 @@
 @objc(BackgroundTimer) class BackgroundTimer : CDVPlugin {
-
-  var onTimerEventCommands: [String] = []
-  let timerInterval: TimeInterval = 9
-
-  private lazy var timer: DispatchSourceTimer = {
-    let t = DispatchSource.makeTimerSource()
-    t.scheduleRepeating(deadline: .now() + self.timerInterval, interval: self.timerInterval)
-    t.setEventHandler(handler: { [weak self] in
-      self?.eventHandler?()
-    })
-    return t
-  }()
-
-  var eventHandler: (() -> Void)?
-
-  private enum State {
-      case suspended
-      case resumed
-  }
-
-  private var state: State = .suspended
-
-  deinit {
-      timer.setEventHandler {}
-      timer.cancel()
-      /*
-        If the timer is suspended, calling cancel without resuming
-        triggers a crash. This is documented here https://forums.developer.apple.com/thread/15902
-        */
-      if state != .resumed {
-          state = .resumed
-          timer.resume()
-      }
-      eventHandler = nil
-  }
-
-  @objc(onTimerEvent:)
-  func onTimerEvent(command: CDVInvokedUrlCommand) {
-    let cmd = command.callbackId;
-    onTimerEventCommands.append(cmd!);
-  }
-  func fireOnTimerEvent() {
-    let pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_OK
-    )
-    pluginResult!.setKeepCallbackAs(true)
-    for itm in onTimerEventCommands {
-      self.commandDelegate!.send(
-        pluginResult,
-        callbackId: itm
-      )
+    
+    var onTimerEventCallbackContext:String?
+    let timerInterval: TimeInterval = 9
+    
+    private lazy var timer: DispatchSourceTimer = {
+        let t = DispatchSource.makeTimerSource()
+        t.scheduleRepeating(deadline: .now() + self.timerInterval, interval: self.timerInterval)
+        t.setEventHandler(handler: { [weak self] in
+            self?.eventHandler?()
+        })
+        return t
+    }()
+    
+    var eventHandler: (() -> Void)?
+    
+    private enum State {
+        case suspended
+        case resumed
     }
-  }
-  @objc(start:)
-  func start(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR,
-      messageAs: "Already started"
-    )
-
-    if state != .resumed {
-      state = .resumed
-      timer.resume()
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: "Started Successfully"
-      )
+    
+    private var state: State = .suspended
+    
+    deinit {
+        timer.setEventHandler {}
+        timer.cancel()
+        /*
+         If the timer is suspended, calling cancel without resuming
+         triggers a crash. This is documented here https://forums.developer.apple.com/thread/15902
+         */
+        if state != .resumed {
+            state = .resumed
+            timer.resume()
+        }
+        eventHandler = nil
     }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-  @objc(stop:)
-  func stop(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR,
-      messageAs: "Already stopped"
-    )
-
-    if state != .suspended {
-      state = .suspended
-      timer.suspend()
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: "Stopped Successfully"
-      )
+    
+    @objc(onTimerEvent:)
+    func onTimerEvent(command: CDVInvokedUrlCommand) {
+        self.onTimerEventCallbackContext = command.callbackId
+        self.commandDelegate.run(inBackground: {
+            let pluginResult:CDVPluginResult = CDVPluginResult(status:CDVCommandStatus_NO_RESULT)
+            pluginResult.setKeepCallbackAs(true)
+            self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
+        })
     }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
+    @objc(start:)
+    func start(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR,
+            messageAs: "Already started"
+        )
+        
+        if state != .resumed {
+            state = .resumed
+            timer.resume()
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: "Started Successfully"
+            )
+        }
+        
+        self.eventHandler = {
+            let pluginResult = CDVPluginResult( status: CDVCommandStatus_OK, messageAs: "Timer fired");
+            pluginResult!.setKeepCallbackAs(true);
+            self.commandDelegate!.send(pluginResult, callbackId:self.onTimerEventCallbackContext)
+        }
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    @objc(stop:)
+    func stop(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR,
+            messageAs: "Already stopped"
+        )
+        
+        if state != .suspended {
+            state = .suspended
+            timer.suspend()
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: "Stopped Successfully"
+            )
+        }
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
 }
